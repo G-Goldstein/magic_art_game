d46e651f1021e3b2b307249b29ca52cfa6c6af72
Graham Goldstein <g.goldstein@live.com>
2015-11-16 16:26:18 +0000

    Brought in from external repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='MagicArtGame',
+      version='1.0',
+      description='Play the Magic art game in Slack!',
+      author='Graham Goldstein',
+      author_email='example@example.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+#      install_requires=['Django>=1.3'],
+     )
